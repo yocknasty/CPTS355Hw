622ed4206642cb6c826ea65c02d1e1cc0e4540e3
yocknasty <ajyockey@gmail.com>
2018-03-25 20:57:10 -0700

    started

---
+++ HW4.py
@@ -0,0 +1,184 @@
+#------- Part 1 TEST CASES--------------
+def testDefine():
+    define("/n1", 4)
+    if lookup("n1") != 4:
+        return False
+    return True
+
+def testLookup():
+    opPush("/n1")
+    opPush(3)
+    psDef()
+    if lookup("n1") != 3:
+        return False
+    return True
+
+#Arithmatic operator tests
+def testAdd():
+    opPush(1)
+    opPush(2)
+    add()
+    if opPop() != 3:
+        return False
+    return True
+
+def testSub():
+    opPush(10)
+    opPush(4.5)
+    sub()
+    if opPop() != 5.5:
+        return False
+    return True
+
+def testMul():
+    opPush(2)
+    opPush(4.5)
+    mul()
+    if opPop() != 9:
+        return False
+    return True
+
+def testDiv():
+    opPush(10)
+    opPush(4)
+    div()
+    if opPop() != 2.5:
+        return False
+    return True
+
+def testMod():
+    opPush(10)
+    opPush(3)
+    mod()
+    if opPop() != 1:
+        return False
+    return True
+
+#Array operator tests
+def testLength():
+    opPush([1,2,3,4,5])
+    length()
+    if opPop() != 5:
+        return False
+    return True
+
+def testGet():
+    opPush([1,2,3,4,5])
+    opPush(4)
+    get()
+    if opPop() != 5:
+        return False
+    return True
+
+#stack manipulation functions
+def testDup():
+    opPush(10)
+    dup()
+    if opPop()!=opPop():
+        return False
+    return True
+
+def testExch():
+    opPush(10)
+    opPush("/x")
+    exch()
+    if opPop()!=10 and opPop()!="/x":
+        return False
+    return True
+
+def testPop():
+    l1 = len(opstack)
+    opPush(10)
+    pop()
+    l2= len(opstack)
+    if l1!=l2:
+        return False
+    return True
+
+def testRoll():
+    opPush(1)
+    opPush(2)
+    opPush(3)
+    opPush(4)
+    opPush(5)
+    opPush(4)
+    opPush(-2)
+    roll()
+    if opPop()!=3 and opPop()!=2 and opPop()!=5 and opPop()!=4 and opPop()!=1:
+        return False
+    return True
+
+def testCopy():
+    opPush(1)
+    opPush(2)
+    opPush(3)
+    opPush(4)
+    opPush(5)
+    opPush(2)
+    copy()
+    if opPop()!=5 and opPop()!=4 and opPop()!=5 and opPop()!=4 and opPop()!=3 and opPop()!=2:
+        return False
+    return True
+
+def testClear():
+    opPush(10)
+    opPush("/x")
+    clear()
+    if len(opstack)!=0:
+        return False
+    return True
+
+#dictionary stack operators
+def testDict():
+    opPush(1)
+    psDict()
+    if opPop()!={}:
+        return False
+    return True
+
+def testBeginEnd():
+    opPush("/x")
+    opPush(3)
+    psDef()
+    opPush({})
+    begin()
+    opPush("/x")
+    opPush(4)
+    psDef()
+    end()
+    if lookup("x")!=3:
+        return False
+    return True
+
+def testpsDef():
+    opPush("/x")
+    opPush(10)
+    psDef()
+    if lookup("x")!=10:
+        return False
+    return True
+
+def testpsDef2():
+    opPush("/x")
+    opPush(10)
+    psDef()
+    opPush(1)
+    psDict()
+    begin()
+    if lookup("x")!=10:
+        end()
+        return False
+    end()
+    return True
+
+
+def main_part1():
+    testCases = [('define',testDefine),('lookup',testLookup),('add', testAdd), ('sub', testSub),('mul', testMul),('div', testDiv),  ('mod', testMod), \
+                ('length', testLength),('get', testGet), ('dup', testDup), ('exch', testExch), ('pop', testPop), ('roll', testRoll), ('copy', testCopy), \
+                ('clear', testClear), ('dict', testDict), ('begin', testBeginEnd), ('psDef', testpsDef), ('psDef2', testpsDef2)]
+    # add you test functions to this list along with suitable names
+    failedTests = [testName for (testName, testProc) in testCases if not testProc()]
+    if failedTests:
+        return ('Some tests failed', failedTests)
+    else:
+        return ('All part-1 tests OK')
